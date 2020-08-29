c2c7cd2d9989141971bd1e12afcdd722a9bd3c3a
Brehannah <57406410+Brehannah@users.noreply.github.com>
2020-08-29 14:12:53 -0400

    contact added

---
+++ about.js
@@ -0,0 +1,89 @@
+
+import React from "react";
+import { Link, Route } from "react-router-dom";
+import Learn from "./Learn";
+
+function Contact(props) {
+  return (
+
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+    <meta charset="UTF-8">
+    <title>CodingPortfolio</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link
+        href="https://fonts.googleapis.com/css?family=Montserrat:200,400,500,600,700|Playfair+Display:400,500,900&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+    <div id="navBarDiv">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-light">
+            <a class="navbar-brand homeButton" href="index.html" style="color: #a7edf9"> <strong>B.HANNAH</strong>
+            <button class="navbar-toggler" type="button" style="color: #a7edf9" data-toggle="collapse"
+                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a href="index.html"><button type="button" class="btn btn-outline-light aboutMePage"
+                                style="color: #a7edf9">ABOUT ME<span class="sr-only">(current)</span></button></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="portfolio.html"><button type="button" class="btn btn-outline-light portfolioPage"
+                                style="color: #a7edf9">PORTFOLIO</button></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="contact.html"><button type="button" class="btn btn-outline-light portfolioPage"
+                                style="color: #a7edf9">CONTACT ME</button></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+        <br>
+        <br>
+        <div class="container">
+            <div class="row row3">
+                <div class="col">
+                    <h2>Contact</h2>
+                    <hr>
+                    <br>
+                    <div>
+                        <form action="#">
+                            <div>Name</div>
+                            <input id="name" class="form-control" type="text" name="Your Name"
+                                placeholder="John Smith">
+                            <br>
+                            <div>Email</div>
+                            <input id="email" class="form-control" type="email" name="Your Email"
+                                placeholder="johnsmith@gmail.com">
+                            <br>
+                            <div>Message</div>
+                            <textarea class="form-control" name="commentBox" id="inputComment" rows="12"></textarea>
+                    </div>
+                    <div>
+                        <button class="button" type="submit">Submit</button>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid">
+
+        </div>
+
+</body>
+
+</html>
+
+
+export default Contact;
