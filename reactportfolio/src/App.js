981e174578d264a7e268059d7a99769b9a2355c2
Brehannah <57406410+Brehannah@users.noreply.github.com>
2020-08-31 18:09:25 -0400

    localhost working

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,25 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react';
+// import './App.css';
+// import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+// import Main from './components/main';
+// import { Link } from 'react-router-dom';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="demo-big-content">
+//     <Layout>
+//         <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">MyPortfolio</Link>} scroll>
+//             <Navigation>
+//                 <Link to="/resume">Resume</Link>
+//                 <Link to="/aboutme">About Me</Link>
+//                 <Link to="/projects">Projects</Link>
+//                 <Link to="/contact">Contact</Link>
+//             </Navigation>
+//         </Header>
+//         <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">MyPortfolio</Link>}>
+//             <Navigation>
+//               <Link to="/resume">Resume</Link>
+//               <Link to="/aboutme">About Me</Link>
+//               <Link to="/projects">Projects</Link>
+//               <Link to="/contact">Contact</Link>
+//             </Navigation>
+//         </Drawer>
+//         <Content>
+//             <div className="page-content" />
+//             <Main/>
+//         </Content>
+//     </Layout>
+// </div>
+
+//     );
+//   }
+// }
+
+// export default App;
+
+
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+
+import Navbar from "./components/Navbar";
+
+import Contact from "./pages/contact";
+import Portfolio from "./pages/portfolio";
+import Index from "./pages/index";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+   
+          <Route exact path="/" component={Index} />
+          <Route exact path="/index" component={Index} />
+          <Route exact path="/contact" component={Contact} />
+          {/* <Route exact path="/about" component={About} /> */}
+          
+          <Route exact path="/portfolio" component={Portfolio} />
+          
+        
+        
+      </div>
+    </Router>
   );
 }
 
---
+++ reactportfolio/src/pages/contact.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+
+const Contact = () => {
+  return (
+    <div>
+      <div className="container" >
+        <div className="row" >
+          <div className="col"><h1>Contact Me</h1></div>
+        </div>
+        <div className="row" >
+          <div className="col-lg-6">
+            <div>Name</div>
+            <input id="name" className="form-control" type="text" name="Your Name"
+              placeholder="Name" />
+            <br />
+            <div>Email</div>
+            <input id="email" className="form-control" type="email" name="Your Email"
+              placeholder="johnsmith@gmail.com" />
+            <br/>
+            
+              <div class="form-group">
+    <label for="exampleFormControlTextarea1">Message</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+
+          </div>
+          </div>
+        </div>
+
+      </div>
+
+  );
+}
+
+export default Contact
